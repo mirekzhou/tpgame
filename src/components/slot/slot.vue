0140f08c49a8515cdecc5796d9ccbf9a135d62ff
mirek <mirek@eveb.com>
2017-05-02 18:10:42 +0800

    slot

--- src/components/slot/slot.vue
+++ src/components/slot/slot.vue
@@ -2,32 +2,42 @@
 	<div class="page slot-page">
 		<div class="page-wrapper home-page-wrapper">
 			<div class="swiper-zone">
-				<div class="left-zone">
-					<span class="left-arrow"></span>
+				<span class="left-arrow"></span>
+
+			    <swiper :options="swiperOption">
+			        <swiper-slide v-for="banner in banners">
+			        	<img :src="banner">
+			        </swiper-slide>
+			    </swiper>
+
+				<span class="right-arrow"></span>
+			</div>
+
+			<div class="platform-zone">
+				<div class="platform-zone-wrapper">
+					<ul>
+						<li v-for="platform in platformList" v-bind:class="[platform.className]" data-platform="platform.value">
+							{{platform.text}}
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="category-zone">
+				<div class="category-zone-wrapper">
+					<ul>
+						<li v-for="category in categoryList" class="active" data-id="category.Id" data-code="category.Code">
+							<span>{{category.Name}}</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="game-zone">
+				<div class="game-zone-left">
 				</div>
 
-				<div class="middle-zone">
-				    <swiper :options="swiperOption">
-				        <swiper-slide>
-				        	<img src="../../assets/slot-banner.png">
-				        </swiper-slide>
-
-				        <swiper-slide>
-				        	<img src="../../assets/slot-banner.png">
-				        </swiper-slide>
-
-				        <swiper-slide>
-				        	<img src="../../assets/slot-banner.png">
-				        </swiper-slide>
-
-				        <swiper-slide>
-				        	<img src="../../assets/slot-banner.png">
-				        </swiper-slide>
-				    </swiper>
-				</div>
-
-				<div class="right-zone">
-					<span class="right-arrow"></span>
+				<div class="game-oper-right">
 				</div>
 			</div>
 		</div>
@@ -37,6 +47,9 @@
 <script>
 	import { mapState } from 'vuex';
 	import { swiper, swiperSlide } from 'vue-awesome-swiper';
+	import slotBanner from '../../assets/slot-banner.png';
+	import Config from '../../config/config.js';
+	import Service from '../../service/service.js';
 
 	export default {
 		name: 'slot-page',
@@ -49,7 +62,55 @@
 			        prevButton:'.left-arrow',
 			        nextButton:'.right-arrow',
 			        spaceBetween: 100
-		        }
+		        },
+
+		        banners: [
+		        	slotBanner,
+		        	slotBanner,
+		        	slotBanner,
+		        	slotBanner
+		        ],
+
+		        platformList :  [
+		        	{
+		        		className: 'platform-PP',
+		        		text: 'PP电子',
+		        		value: 'PP'
+		        	},
+		        	{
+		        		className: 'platform-PT',
+		        		text: 'PT电子',
+		        		value: 'PT'
+		        	},
+		        	{
+		        		className: 'platform-BBIN',
+		        		text: 'BBIN电子',
+		        		value: 'BBIN'
+		        	},
+		        	{
+		        		className: 'platform-MG',
+		        		text: 'MG电子',
+		        		value: 'MG'
+		        	},
+		        	{
+		        		className: 'platform-AG',
+		        		text: 'AG电子',
+		        		value: 'AG'
+		        	},
+		        	{
+		        		className: 'platform-TTG',
+		        		text: 'TTG电子',
+		        		value: 'TTG'
+		        	},
+		        	{
+		        		className: 'platform-MT',
+		        		text: 'MT电子',
+		        		value: 'MT'
+		        	}
+		        ],
+		        categoryList      :  [],
+		        gameList          :  [],
+		        favoriteGameList  :  []
 			}
 		},
 
@@ -59,6 +120,115 @@
 		},
 
 		methods: {
+			getBanners: function () {
+				var callback;
+				var that    =  this;
+				var opt     =  {
+					url: Config.urls.getAds,
+					data: {
+						type: 'pd_wyb_slot_ads',
+						pageIndex: 0,
+						pageSize: 10
+					}
+				};
+
+				callback = function (data) {
+					that.loader4.stop();
+					
+					if (!data) {
+						return;
+					}
+					
+					that.addSliders(data);
+				};
+
+				Service.get(opt, callback);
+			},
+
+			getJackpotsGames: function (platform) {
+				var callback;
+				var eJackpotsGames;
+				var that =  this;
+				var opt  =  {
+					url: app.urls.getJackpotsGames,
+			        data: {
+			        	platform: platform,
+			        	pageIndex: 0,
+			        	pageSize: 25
+			        }
+				};
+
+				if (platform !== 'PT' && platform !== 'MG') {
+					return;
+				}
+
+				eJackpotsGames = localStorage.getItem('e-jackpots-games');
+
+				if (eJackpotsGames) {
+		        	this.loader1.stop();
+		        	this.bonusPoolData = JSON.parse(eJackpotsGames);
+		        	this.setMarqueenItems(true);
+		        	return;
+				}
+
+				callback = function (json) {
+		        	that.bonusPoolData = json;
+		        	localStorage.setItem('e-jackpots-games', JSON.stringify(json));
+		        	that.loader1.stop();
+		        	that.setMarqueenItems(true);
+				};
+
+				this.loader1.play();
+				Service.get(opt, callback);
+			},
+
+			getCategoryList: function () {
+				var opt;
+				var callback;
+				var that = this;
+				var eGameCategoryData = localStorage.getItem('e-game-category');
+
+				if (eGameCategoryData) {
+		        	this.categoryList = JSON.parse(eGameCategoryData);
+		        	this.getFavoriteGameIds();
+		        	return;
+				}
+
+				opt = {
+					url: Config.urls.getGameCategories,
+					data: {
+						code: 'electron'
+					}
+				};
+
+				callback = function (json) {
+					if (json.StatusCode && json.StatusCode != 0) {
+						alert(json.Message);
+						return;
+					}
+
+					localStorage.setItem('e-game-category', JSON.stringify(json));
+		        	that.categoryList = json;
+		        	console.log('categoryList = ' + JSON.parse(that.categoryList));
+					that.getFavoriteGameIds();
+				};
+
+				Service.get(opt, callback);
+			},
+
+			getFavoriteGameIds: function () {
+				if (this.loginStatus) {
+
+				} else {
+
+				}
+			}
+		},
+
+		mounted: function () {
+			//this.getBanners();
+			//this.getJackpotsGames('PT');
+			this.getCategoryList();
 		},
 
 	  	computed: mapState({
@@ -70,41 +240,38 @@
 </script>
 
 <style lang="scss" scoped>
+	$slotWrapperL            :    1140;
+	$slotWrapperWidth        :    1140px;
+	$gameLeftWidth           :    $slotWrapperWidth * 895 / $slotWrapperL;
+	$gameRightWidth          :    $slotWrapperWidth * 230 / $slotWrapperL;
+	$platformZoneHeight      :    $slotWrapperWidth * 66 / $slotWrapperL;
+	$categoryZoneHeight      :    $slotWrapperWidth * 66 / $slotWrapperL;
+
 	.slot-page {
 		.swiper-zone {
-			display: flex;
+		    -webkit-user-select: none;
+		    user-select: none;
 			position: relative;
 			width: 100%;
 
-			.left-zone {
-				position: absolute;
-				left: 0;
-				top: 0;
-				height: 100%;
+			.left-arrow {
+				background-image: url(../../assets/page-left-arrow.png);
+				cursor: pointer;
+				height: 60px;
 				width: 60px;
-				display: flex;
-				flex-direction: row;
-				justify-content: space-around;
-				align-items: center;
+			    position: absolute;
+			    top: 50%;
+			    margin-top: -30px;
+			    left: 0;
+			    z-index: 2;
 
-				.left-arrow {
-					background-image: url(../../assets/page-left-arrow.png);
-					cursor: pointer;
-					height: 60px;
-					width: 60px;
-
-					&:hover {
-						background-image: url(../../assets/page-left-arrow-active.png);
-					}
+				&:hover {
+					background-image: url(../../assets/page-left-arrow-active.png);
 				}
 			}
 
 			.middle-zone {
-				position: absolute;
-				left: 0;
-				right: 0;
-				top: 0;
-				height: 100%;
+				height: auto;
 
 				.swiper-slide {
 					img {
@@ -113,27 +280,91 @@
 				}
 			}
 
-			.right-zone {
-				position: absolute;
-				right: 0;
-				top: 0;
-				height: 100%;
+			.right-arrow {
+				background-image: url(../../assets/page-right-arrow.png);
+				cursor: pointer;
+				height: 60px;
 				width: 60px;
-				display: flex;
-				flex-direction: row;
-				justify-content: space-around;
-				align-items: center;
+   				position: absolute;
+			    top: 50%;
+			    margin-top: -30px;
+			    right: 0;
+			    z-index: 2;
 
-				.right-arrow {
-					background-image: url(../../assets/page-right-arrow.png);
-					cursor: pointer;
-					height: 60px;
-					width: 60px;
+				&:hover {
+					background-image: url(../../assets/page-right-arrow-active.png);
+				}
+			}
+		}
 
-					&:hover {
-						background-image: url(../../assets/page-right-arrow-active.png);
+		.platform-zone {
+			background-color: rgba(0, 0, 0, 1);
+			height: $platformZoneHeight;
+			margin-top: 0 - $platformZoneHeight;
+			width: 100%;
+
+			.platform-zone-wrapper {
+				width: $slotWrapperWidth;
+				margin: 0 auto;
+
+				ul {
+					list-style: none;
+
+					li {
+						cursor: pointer;
+						display: inline-block;
+						height: $platformZoneHeight;
+						line-height: $platformZoneHeight;
+						width: 140px;
+						text-align: center;
 					}
 				}
+			}
+		}
+
+		.category-zone {
+			background-color: rgba(0, 0, 0, .1);
+			height: $categoryZoneHeight;
+			width: 100%;
+
+			.category-zone-wrapper {
+				margin: 0 auto;
+				width: $slotWrapperWidth;
+
+				ul {
+					list-style: none;
+
+					li {
+						cursor: pointer;
+						display: inline-block;
+						font-size: 14px;
+						height: $platformZoneHeight;
+						line-height: $platformZoneHeight;
+						width: auto;
+						margin-right: 60px;
+						text-align: left;
+					}
+
+					.active {
+						span {
+							background-color: #322b4d;
+							border-radius: 30px;
+							padding: 10px 25px;
+						}
+					}
+				}
+			}
+		}
+
+		.game-zone {
+			width: $slotWrapperWidth;
+
+			.game-zone-left {
+				width: $gameLeftWidth;
+			}
+
+			.game-zone-right {
+				width: $gameRightWidth;
 			}
 		}
 	}
---
+++ src/components/slot/slotGameItem.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="slot-game-item">
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'slot-game-item',
+		
+		props: [
+			'id',            //游戏Id
+			'identify',      //游戏Identity
+			'isTry',         //是否可以试玩
+			'gameType',      //游戏类型
+			'platform',      //游戏平台
+			'collectid',     //收藏Id
+			'collected',     //是否已经被收藏
+			'cnname',        //游戏中文名称
+			'showJackpots',  //是否显示奖金池
+			'jackpotUrl',    //jackpot奖金池url
+			'imageUrl'       //游戏图片url
+		],
+
+		data: function () {
+			return {
+			}
+		},
+
+		components: {
+		},
+
+		methods: {
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	$gameL                   :    1140;
+	$gameWidth               :    1140px;
+	$gameItemWidth           :    $gameWidth * 170 / $gameL;
+	$gameItemHeight          :    $gameWidth * 146 / $gameL;
+
+	.slot-game-item {
+		border: 1px solid #5c5674;
+		height: $gameItemHeight;
+		width: $gameItemWidth;
+	}
+</style>
