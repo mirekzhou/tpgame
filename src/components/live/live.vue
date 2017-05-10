46c50b35ba7b16c7550c0cabb0711ba5f82869dc
mirek <mirek@eveb.com>
2017-05-10 18:06:04 +0800

    live

--- src/components/live/live.vue
+++ src/components/live/live.vue
@@ -12,11 +12,16 @@
 
 				<span class="right-arrow"></span>
 			</div>
+
+			<div class="platforms-zone"></div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import { swiper, swiperSlide } from 'vue-awesome-swiper';
+	import liveBanner from '../../assets/live-banner.png';
+
 	export default {
 		name: 'live-page',
 		
@@ -29,8 +34,20 @@
 			        nextButton:'.right-arrow',
 			        spaceBetween: 100,
 			        autoHeight: true
-		        }
+		        },
+
+		        banners: [
+		        	liveBanner,
+		        	liveBanner,
+		        	liveBanner,
+		        	liveBanner
+		        ]
 			}
+		},
+
+		components: {
+			'swiper'         : swiper,
+			'swiper-slide'   : swiperSlide
 		},
 
 		methods: {
@@ -40,5 +57,54 @@
 
 <style lang="scss" scoped>
 	.live-page {
+		.swiper-zone {
+		    -webkit-user-select: none;
+		    user-select: none;
+			position: relative;
+			width: 100%;
+
+			.left-arrow {
+				background-image: url(../../assets/page-left-arrow.png);
+				cursor: pointer;
+				height: 60px;
+				width: 60px;
+			    position: absolute;
+			    top: 50%;
+			    margin-top: -30px;
+			    left: 0;
+			    z-index: 2;
+
+				&:hover {
+					background-image: url(../../assets/page-left-arrow-active.png);
+				}
+			}
+
+			.middle-zone {
+				height: auto;
+			}
+
+			.right-arrow {
+				background-image: url(../../assets/page-right-arrow.png);
+				cursor: pointer;
+				height: 60px;
+				width: 60px;
+   				position: absolute;
+			    top: 50%;
+			    margin-top: -30px;
+			    right: 0;
+			    z-index: 2;
+
+				&:hover {
+					background-image: url(../../assets/page-right-arrow-active.png);
+				}
+			}
+		}
+
+		.platforms-zone {
+			background-image: url('../../assets/live-bg.png');
+			width: 100%;
+			min-height: 800px;
+			margin-top: -5px;
+		}
 	}
 </style>
